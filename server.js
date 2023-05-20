68c36ace99d4c369564a5ee42ab77f476d769565
Pierre-Marie Marchio <pierremariemarchio@gmail.com>
2023-05-20 19:19:48 +0200

    server basic implementation

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const https = require("https");
 const path = require("path");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, ".", "public", "html", "index.html"));
+});
+
+app.post("/", (req, res) => {
+  
+  var Subscribers = {
+    firstName: req.body.fName,
+    lastName: req.body.lName,
+    email: req.body.email
+  };
+  console.log(Subscribers);
 });
 
 app.listen("3000", (req, res) => {
