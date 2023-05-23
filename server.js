49eb87e8b4ec8743c9f3f8ef52402f36989ff03f
Pierre-Marie Marchio <pierremariemarchio@gmail.com>
2023-05-23 20:09:20 +0200

    api

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+// ****** Pakages ****** //
+
+require('dotenv').config({ path: './env/.env' })
+
 const express = require("express");
 const https = require("https");
 const path = require("path");
 const bodyParser = require("body-parser");
 
+
+// ****** Varriables ****** //
+
 const app = express();
+const API_Key = process.env.API_KEY;
+const audience_ID = process.env.AUDIENCE_ID;
+
+// ****** Server ****** //
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -19,9 +30,40 @@ app.post("/", (req, res) => {
     lastName: req.body.lName,
     email: req.body.email
   };
-  console.log(Subscribers);
-});
 
-app.listen("3000", (req, res) => {
-  console.log("listening on http://localhost 3000");
+  var data = {
+    members : [
+      {
+        email_address: Subscribers.email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: Subscribers.firstName,
+          LNAME: Subscribers.lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data)
+  const url = `https://us14.api.mailchimp.com/3.0/lists/${audience_ID}`;
+  options = {
+    methode: "POST",
+    auth: `user:${API_Key}`
+  };
+
+  
+
+  const request = https.request(url, options, (res) => {
+    res.on("data", (data) => {
+      console.log(JSON.parse(data));
+    })
+  })
+
+  request.write(jsonData);
+  request.end();
+  
+});
+ 
+app.listen("3000", () => {
+  console.log("server running on port : 3000");
 });
