c48dbc725e7433ae9c89582adc5b779bbd6bb784
Pierre-Marie Marchio <pierremariemarchio@gmail.com>
2023-05-24 15:22:29 +0200

    finished no bug

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 // ****** Pakages ****** //
 
-require('dotenv').config({ path: './env/.env' })
+require("dotenv").config({ path: "./env/.env" });
 
 const express = require("express");
 const https = require("https");
 const path = require("path");
 const bodyParser = require("body-parser");
-
 
 // ****** Varriables ****** //
 
@@ -24,46 +23,56 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-  
   var Subscribers = {
     firstName: req.body.fName,
     lastName: req.body.lName,
-    email: req.body.email
+    email: req.body.email,
   };
 
   var data = {
-    members : [
+    members: [
       {
         email_address: Subscribers.email,
         status: "subscribed",
         merge_fields: {
           FNAME: Subscribers.firstName,
-          LNAME: Subscribers.lastName
-        }
-      }
-    ]
+          LNAME: Subscribers.lastName,
+        },
+      },
+    ],
   };
 
-  const jsonData = JSON.stringify(data)
+  const jsonData = JSON.stringify(data);
   const url = `https://us14.api.mailchimp.com/3.0/lists/${audience_ID}`;
+
   options = {
-    methode: "POST",
-    auth: `user:${API_Key}`
+    method: "POST",
+    auth: `user:${API_Key}`,
   };
 
-  
+  const request = https.request(url, options, (response) => {
 
-  const request = https.request(url, options, (res) => {
-    res.on("data", (data) => {
+    if (response.statusCode === 200) {
+      
+      res.sendFile(path.join(__dirname, ".", "public", "html", "success.html"));
+      
+    } else {
+      res.sendFile(path.join(__dirname, ".", "public", "html", "failure.html"));
+    }
+
+    response.on("data", (data) => {
       console.log(JSON.parse(data));
-    })
-  })
-
+    });
+  });
   request.write(jsonData);
   request.end();
-  
 });
- 
+
+app.post("/failure", (req, res) => {
+  res.redirect("/")
+})
+
+
 app.listen("3000", () => {
   console.log("server running on port : 3000");
 });
